1b8f5b80ae108ed9c4992588e53cd67fd73f90b7
cnanduri <nlkchaitanya@gmail.com>
2017-03-06 10:59:30 -0500

    Adding Preferences via settings

--- app/src/main/java/app/udacity/android/cn/sunshine/DetailActivityFragment.java
+++ app/src/main/java/app/udacity/android/cn/sunshine/DetailActivityFragment.java
@@ -4,7 +4,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -14,7 +20,13 @@ import android.widget.TextView;
  */
 public class DetailActivityFragment extends Fragment {
 
-    private String mTextMsg;
+    private static final String LOG_TAG = DetailActivityFragment.class.getSimpleName();
+
+    private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
+
+    private ShareActionProvider mShareActionProvider;
+
+    private String mForecastStr;
 
     public DetailActivityFragment() {
     }
@@ -23,7 +35,21 @@ public class DetailActivityFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Intent intent = getActivity().getIntent();
-        mTextMsg = intent.getStringExtra(Intent.EXTRA_TEXT);
+        mForecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+
+        // Locate MenuItem with ShareActionProvider
+        MenuItem item = menu.findItem(R.id.action_share);
+
+        // Fetch and store ShareActionProvider
+        mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+
+        setShareForecastIntent(createForecastShareIntent());
     }
 
     @Override
@@ -32,7 +58,25 @@ public class DetailActivityFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
 
         TextView textView = (TextView) rootView.findViewById(R.id.detailActivitytextView);
-        textView.setText(mTextMsg);
+        textView.setText(mForecastStr);
         return rootView;
+    }
+
+    // Call to update the share intent
+    private void setShareForecastIntent(Intent shareIntent) {
+        if (mShareActionProvider != null) {
+            mShareActionProvider.setShareIntent(shareIntent);
+        } else {
+            Log.d(LOG_TAG, "ShareActionProvider is null");
+        }
+    }
+
+    private Intent createForecastShareIntent() {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.putExtra(Intent.EXTRA_TEXT, mForecastStr + FORECAST_SHARE_HASHTAG)
+                .setType("text/plain")
+                .setFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT);
+        setShareForecastIntent(shareIntent);
+        return shareIntent;
     }
 }
