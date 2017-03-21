ec5564d70fdf4b18e4b3a93c9f0c496589556440
cnanduri <nlkchaitanya@gmail.com>
2017-03-21 11:26:23 -0400

    Quiz : Building List Item

--- app/src/main/java/app/udacity/android/cn/sunshine/ForecastAdapter.java
+++ app/src/main/java/app/udacity/android/cn/sunshine/ForecastAdapter.java
@@ -6,9 +6,6 @@ import android.support.v4.widget.CursorAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-
-import app.udacity.android.cn.sunshine.data.WeatherContract;
 
 /**
  * {@link ForecastAdapter} exposes a list of weather forecasts
@@ -36,8 +33,8 @@ public class ForecastAdapter extends CursorAdapter {
     public void bindView(View view, Context context, Cursor cursor) {
         // our view is pretty simple here --- just a text view
         // we'll keep the UI functional with a simple (and slow!) binding.
-        TextView tv = (TextView)view;
-        tv.setText(convertCursorRowToUXFormat(cursor));
+        /*TextView tv = (TextView)view;
+        tv.setText(convertCursorRowToUXFormat(cursor));*/
     }
 
     /**
